acf7821d86acabc61e9258e390ee6eae40787584
jenkins <jenkins@example.com>
2020-10-27 15:15:59 +0000

    Fixing pathes. Now .sh files should be executed inside repo folder

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install nginx -y
 sudo service nginx stop
 sudo rm -rf /var/www/html/*
-sudo cp devops-practice-final-project/html/* /var/www/html/
-sudo cp devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
+sudo cp html/* /var/www/html/
+sudo cp nginx/conf/default /etc/nginx/sites-enabled/default
 sudo service nginx start
-sudo chmod +x devops-practice-final-project/update.sh
+sudo chmod +x update.sh
