38c2ba012114c5af3a9e9b542dade261cbe8572f
jenkins <jenkins@example.com>
2020-10-27 13:23:57 +0000

    Add update.sh + small fixes

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ sudo rm -rf /var/www/html/*
 sudo cp devops-practice-final-project/html/* /var/www/html/*
 sudo cp devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
 sudo service nginx start
+sudo chmod +x devops-practice-final-project/update.sh
--- update.sh
+++ update.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-sudo apt update
-sudo apt install nginx -y
-sudo service nginx stop
-sudo rm -rf /var/www/html/*
-sudo cp devops-practice-final-project/html/* /var/www/html/*
-sudo cp devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
-sudo service nginx start
-
 mkdir temp-dir
 git clone https://github.com/Leksandrua/devops-practice-final-project.git temp-dir/
 sudo rm -rf /var/www/html/*
