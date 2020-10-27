7bb7cbe0aff7c1109ae36d4ce84656aca91f36e0
jenkins <jenkins@example.com>
2020-10-27 13:20:44 +0000

    Change directory hierarchy + modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install nginx -y
+sudo service nginx stop
 sudo rm -rf /var/www/html/*
-sudo cp devops-practice-final-project/index.html /var/www/html/index.html
+sudo cp devops-practice-final-project/html/* /var/www/html/*
 sudo cp devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
+sudo service nginx start
