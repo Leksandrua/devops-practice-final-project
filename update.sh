6b5682f341a6db0695c2a41da0ea9cdcb78f14fe
jenkins <jenkins@example.com>
2020-10-27 13:33:27 +0000

    Small fixes x2

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 mkdir temp-dir
 git clone https://github.com/Leksandrua/devops-practice-final-project.git temp-dir/
 sudo rm -rf /var/www/html/*
-sudo cp temp-dir/devops-practice-final-project/html/* /var/www/html/
-sudo cp temp-dir/devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
+sudo cp temp-dir/html/* /var/www/html/
+sudo cp temp-dir/nginx/conf/default /etc/nginx/sites-enabled/default
 sudo rm -rf temp-dir
 sudo service nginx start
