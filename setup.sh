d545011b16d2caca0efb3290a2329803dd947a8c
jenkins <jenkins@example.com>
2020-10-27 17:14:56 +0000

    Checks, that everything works perfectly

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ export INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.1
 export ETH0_MAC=$(ip -o link show dev eth0 | grep -Po 'ether \K[^ ]*')
 export SUBNET_ID=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$ETH0_MAC/subnet-id)
 
-echo "Hello from instance $INSTANCE_ID, running in subnet $SUBNET_ID" > html/index.html
+echo "Hello from instance $INSTANCE_ID, running in subnet $SUBNET_ID" >> html/index.html
 
 sudo cp html/* /var/www/html/
 sudo cp nginx/conf/default /etc/nginx/sites-enabled/default
