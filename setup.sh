dba5018a52e3cb30d5f1fc3536bd2b413656e797
jenkins <jenkins@example.com>
2020-10-27 13:12:23 +0000

    Add some changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
 sudo apt install nginx -y
-sudo rm -rf /var/www/html*
-sudo cp index.html /var/www/html/index.html
-sudo cp nginx/conf/default /etc/nginx/sites-enabled/default
+sudo rm -rf /var/www/html/*
+sudo cp devops-practice-final-project/index.html /var/www/html/index.html
+sudo cp devops-practice-final-project/nginx/conf/default /etc/nginx/sites-enabled/default
