91b55ff27093379d821685146b7e614cfdfe5968
jenkins <jenkins@example.com>
2020-10-27 15:57:01 +0000

    Fixing typo

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo rm -rf /var/www/html/*
 
 export INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance->
 export ETH0_MAC=$(ip -o link show dev eth0 | grep -Po 'ether \K[^ ]*')
-export SUBNET_ID=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl->
+export SUBNET_ID=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$ETH0_MAC/subnet-id)
 
 echo "Hello from instance $INSTANCE_ID, running in subnet $SUBNET_ID" > html/index.html
 
